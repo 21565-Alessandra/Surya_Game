b9a941e14eac16f55dd1de74cf0f297abbc1a6b5
Alessandra Reis <alereis1002@gmail.com>
2022-04-13 23:05:21 +0100

    surya_lvl1
    Adding background and star script

---
+++ Surya_Game/Assets/Scripts/Star.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Star : MonoBehaviour
+{
+    public float speed = 120f;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float h = Input.GetAxisRaw("Horizontal"); //store x axis movement as float
+
+        GetComponent<Rigidbody2D>().velocity = Vector2.right * h * speed;
+        
+    }
+}
